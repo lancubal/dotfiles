4029b096ff8d26d50b7f373dc602af5290861cc7
lancubal <lunalancuba.sistemas@gmail.com>
2026-06-13 10:33:29 -0300

    chore: add coursier bin directory to fish path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,7 @@ function fish_greeting
 end
 
 starship init fish | source
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/home/luna/.local/share/coursier/bin"
+# <<< coursier install directory <<<
